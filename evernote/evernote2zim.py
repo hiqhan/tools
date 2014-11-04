c93386310aa36f5ceb469a59df1e92dc8942459d
hiqhan <qiuhan.h@gmail.com>
2014-11-04 12:41:50 +0800

    evernote2zim
    Convert shared links with titles from evernote into link format in Zim
    Desktop Wiki.

---
+++ evernote/evernote2zim.py
@@ -0,0 +1,37 @@
+import re
+import sys
+
+__author__ = 'qhanhuang'
+html_filename = 'Evernote.html'
+links_filename = 'SharedLinks.txt'
+output_filename = 'evernote.txt'
+
+if __name__ == "__main__":
+	# read from exported html
+    with open(html_filename) as html_file:
+        s = html_file.read()
+	# get shared links from file
+    with open(links_filename, 'r') as link_file:
+        links = link_file.readlines()
+
+	# get title
+    start = '<h1>'
+    end = '</h1>'
+    title = re.findall(re.escape(start)+"(.*)"+re.escape(end), s)
+
+	# the number of titles should be equal to links'
+    if len(title) != len(links):
+        print "title lines are not equal to links"
+        sys.exit(0)
+
+    else:
+        res = ["* [[" + links[i].strip() + "|" + title[i] + "]]" for i in range(len(title))]
+
+	# output the zim style
+    with open(output_filename, 'w') as output:
+        output.writelines('\n'.join(res))
+
+
+
+
+
